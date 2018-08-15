e9ef893c1400aabb5a7e5e8f256e4341f0c4d341
Josh Kennedy <joshkennedy77@gmail.com>
2018-08-14 18:02:55 -0700

    initial push

---
+++ rg architecture _ exceptional building design_files/css.php
@@ -0,0 +1,36 @@
+
+																		
+
+	ul.slideshowgallery4360 { list-style:none !important; color:#fff; }
+	ul.slideshowgallery4360 span { display:none; }
+	#slideshow-wrappergallery4360 { overflow:hidden; position:relative; width:960px; background:#ffffff; padding:0 0 0 0; border:#fff; margin:0; display:none; }
+	#slideshow-wrappergallery4360 * { margin:0; padding:0; }
+	#slideshow-wrappergallery4360 #fullsizegallery4360 { position:relative; z-index:1; overflow:hidden; width:960px; height:400px; clear:both; border: none; }
+	#slideshow-wrappergallery4360 #informationgallery4360 { text-align:left; font-family:Verdana, Arial, Helvetica, sans-serif !important; position:absolute; bottom:0; width:960px; height:0; background:#ffffff; color:#ffffff; overflow:hidden; z-index:300; opacity:.7; filter:alpha(opacity=70); }
+	#slideshow-wrappergallery4360 #informationgallery4360 h3 { color:#ffffff; padding:4px 8px 3px; margin:0 !important; font-size:16px; font-weight:bold; }
+	#slideshow-wrappergallery4360 #informationgallery4360 a { color:#ffffff; }
+	#slideshow-wrappergallery4360 #informationgallery4360 p { color:#ffffff; padding:0 8px 8px; margin:0 !important; font-size: 14px; font-weight:normal; }
+	#slideshow-wrappergallery4360 #imagegallery4360 { width:960px; }
+	#slideshow-wrappergallery4360 #imagegallery4360 img { border:none; border-radius:0; box-shadow:none; position:absolute; height:auto; max-width:100%; margin:0 auto; display:block; }
+	#slideshow-wrappergallery4360 .imgnav { position:absolute; width:25%; height:100%; cursor:pointer; z-index:250; }
+	#slideshow-wrappergallery4360 #imgprevgallery4360:before { font-family:FontAwesome; content:"\f053"; font-size:30px; color:white; visibility:visible; left:0; text-align:left; width: auto; height:auto; line-height:160%; top:50%; margin:-30px 0 0 0; border-radius:0 10px 10px 0; background:black; padding:3px 10px 0 10px; position: absolute; display: inline-block; 	}
+	#slideshow-wrappergallery4360 #imgprevgallery4360 { display:none; -moz-user-select: none; -khtml-user-select: none; -webkit-user-select: none; -o-user-select: none; left:0; font-size:0px; }
+	#slideshow-wrappergallery4360 #imgnextgallery4360:before { font-family:FontAwesome; content:"\f054"; font-size:30px; color:white; visibility:visible; right:0; text-align:right; width: auto; height:auto; line-height:160%; top:50%; margin:-30px 0 0 0; border-radius:10px 0 0 10px; background:black; padding:3px 10px 0 10px; position: absolute; display: inline-block; }
+	#slideshow-wrappergallery4360 #imgnextgallery4360 { display:none; -moz-user-select: none; -khtml-user-select: none; -webkit-user-select: none; -o-user-select: none; right:0; font-size:0px; }
+	#slideshow-wrappergallery4360 #imglinkgallery4360 { position:absolute; zoom:1; background-color:#ffffff; height:100%; width:100%; left:0; z-index:149; opacity:0; filter:alpha(opacity=0); }
+	#slideshow-wrappergallery4360 .linkhover:before { font-family:FontAwesome; content:"\f14c"; font-size:30px; text-align:center; height:auto; line-height:160%; width:auto; top:50%; left:auto; right:auto; margin:-30px 0 0 0; padding:0px 12px; display: inline-block; position: relative; background:black; color:white; border-radius:10px; }
+	#slideshow-wrappergallery4360 .linkhover { background:transparent !important; opacity:.4 !important; filter:alpha(opacity=40) !important; text-align:center; font-size:0px; }
+	#slideshow-wrappergallery4360 #thumbnailsgallery4360 { background:#ffffff; }
+	#slideshow-wrappergallery4360 .thumbstop { margin-bottom:8px !important; }
+	#slideshow-wrappergallery4360 .thumbsbot { margin-top:8px !important; }
+	#slideshow-wrappergallery4360 #slideleftgallery4360:before { font-family:FontAwesome; content: "\f104"; color:#999; position: absolute; height: auto; line-height:160%; width: auto; display: inline-block; top: 50%; font-size: 30px; margin: -30px 0 0 0; padding: 0 5px; }
+	#slideshow-wrappergallery4360 #slideleftgallery4360 { float:left; position:relative; width:20px; height:54px; background:#222; }
+	#slideshow-wrappergallery4360 #slideleftgallery4360:hover { background-color:#333; }
+	#slideshow-wrappergallery4360 #sliderightgallery4360:before { font-family:FontAwesome; content: "\f105"; color:#999; position: absolute; height: auto; line-height:160%; width: auto; display: inline-block; top: 50%; font-size: 30px; margin: -30px 0 0 0; padding: 0 5px; }
+	#slideshow-wrappergallery4360 #sliderightgallery4360 { float:right; position:relative; width:20px; height:54px; background:#222; }
+	#slideshow-wrappergallery4360 #sliderightgallery4360:hover { background-color:#333; }
+	#slideshow-wrappergallery4360 #slideareagallery4360 { float:left; position:relative; background:#ffffff; width:905px; margin:0 5px; height:54px; overflow:hidden; }
+	#slideshow-wrappergallery4360 #slidergallery4360 { position:absolute; width:1196px !important; left:0; height:54px; }
+	#slideshow-wrappergallery4360 #slidergallery4360 img { cursor:pointer; border:1px solid #666; padding:2px; float:left !important; }
+	#slideshow-wrappergallery4360 #spinnergallery4360 { position:relative; top:50%; left:45%; text-align:left; font-size:30px; }	
+	#slideshow-wrappergallery4360 #spinnergallery4360 img { border:none; }
